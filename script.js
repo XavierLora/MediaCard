f0bca80abf42e70b52a551852f9c07716dee4904
Xavier Lora <104650618+XavierLora@users.noreply.github.com>
2023-09-28 12:26:40 -0400

    MediaCard Updates
    Major overall, js, and image fixes

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById("myvid").addEventListener("mouseover",()=>{document.getElementById("myvid").play()});
+document.getElementById("myvid").addEventListener("mouseleave",()=>{document.getElementById("myvid").pause()});
