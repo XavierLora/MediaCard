5455da7d19fdc8076c14ba0fde229f963be4d8df
Xavier Lora <104650618+XavierLora@users.noreply.github.com>
2023-10-03 12:35:18 -0400

    Adding Css and html to get layout for MediaCard

--- script.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("myvid").addEventListener("mouseover",()=>{document.getElementById("myvid").play()});
-document.getElementById("myvid").addEventListener("mouseleave",()=>{document.getElementById("myvid").pause()});
